6e1ef9e0652fde9b4afd5e8dcdd4b8645de82138
Vuldis <vlad.sechenov@mail.ru>
2022-08-18 00:55:20 +0700

    feature(NotesAppUI): добавил несколько объектов textbox и label

--- NotesAppUI/Form1.Designer.cs
+++ NotesAppUI/Form1.Designer.cs
@@ -28,20 +28,116 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 47);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(101, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Show Category:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(119, 44);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(170, 24);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(15, 87);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(274, 326);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Enabled = false;
+			this.richTextBox2.Location = new System.Drawing.Point(318, 87);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(470, 326);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(315, 47);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 16);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Created:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(525, 47);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(62, 16);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Modified:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(315, 21);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(65, 16);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Category:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(386, 21);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(114, 16);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Name of category";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "NotesApp";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
 	}
 }
 
