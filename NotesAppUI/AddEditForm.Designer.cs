630d8a720ad820c11b4e14bcdb7cec72853c9c4f
Vuldis <vlad.sechenov@mail.ru>
2022-08-20 15:53:26 +0700

    feature(AddEditForm): добавил textBox для ввода названия заметки, ComboBox для выбора категории

--- NotesAppUI/AddEditForm.Designer.cs
+++ NotesAppUI/AddEditForm.Designer.cs
@@ -28,12 +28,66 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.AddCategoryComboBox = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(36, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Title:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(84, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(513, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 53);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Category:";
+			// 
+			// AddCategoryComboBox
+			// 
+			this.AddCategoryComboBox.FormattingEnabled = true;
+			this.AddCategoryComboBox.Location = new System.Drawing.Point(85, 53);
+			this.AddCategoryComboBox.Name = "AddCategoryComboBox";
+			this.AddCategoryComboBox.Size = new System.Drawing.Size(121, 24);
+			this.AddCategoryComboBox.TabIndex = 3;
+			// 
+			// AddEditForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "AddEditForm";
+			this.ClientSize = new System.Drawing.Size(611, 504);
+			this.Controls.Add(this.AddCategoryComboBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "AddEditForm";
+			this.Text = "Add/Edit Note";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox AddCategoryComboBox;
 	}
 }
