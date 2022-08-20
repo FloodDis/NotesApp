264bcdd7c83b2a328907cf55d7a31f0e50b4b103
Vuldis <vlad.sechenov@mail.ru>
2022-08-20 23:02:38 +0700

    feature(NotesAppUI): создал класс заметки Note

---
+++ NotesAppUI/Note.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAppUI
+{
+	/// <summary>
+	/// Заметка
+	/// </summary>
+	internal class Note
+	{
+		/// <summary>
+		/// Название заметки
+		/// </summary>
+		private string _name;
+
+		/// <summary>
+		/// Категория заметки
+		/// </summary>
+		private Category _category;
+
+		/// <summary>
+		/// Текст заметки
+		/// </summary>
+		private string _text;
+
+		/// <summary>
+		/// Время создания заметки
+		/// </summary>
+		private DateTime _creationTime;
+
+		/// <summary>
+		/// Время изменения заметки
+		/// </summary>
+		private DateTime _modificationTime;
+
+	}
+}
