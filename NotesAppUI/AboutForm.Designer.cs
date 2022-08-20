cf96b4d19d50d2e8b9a83271707153e137d0796a
Vuldis <vlad.sechenov@mail.ru>
2022-08-20 16:11:38 +0700

    feature(AboutForm): убрал отображение иконки

--- NotesAppUI/AboutForm.Designer.cs
+++ NotesAppUI/AboutForm.Designer.cs
@@ -133,6 +133,7 @@
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Name = "AboutForm";
+			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "About";
 			this.ResumeLayout(false);
