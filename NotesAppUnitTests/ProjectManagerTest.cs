4834aa17c2ce2266c116ddfeea6263368b440e6e
Vlad Sechenov <vlad.sechenov@mail.ru>
2022-12-18 03:18:57 +0700

    refactoring(NotesApp.UnitTests): изменен формат указания пространства имен и написаны global usings

--- NotesAppUnitTests/ProjectManagerTest.cs
+++ NotesAppUnitTests/ProjectManagerTest.cs
@@ -3,43 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
-using NotesAppClasses;
 
-namespace NotesApp.UnitTests
+namespace NotesApp.UnitTests;
+
+[TestFixture]
+public class ProjectManagerTest
 {
-	[TestFixture]
-	public class ProjectManagerTest
+	[Test(Description = "Тест сохранения и загрузки списка заметок")]
+	public void TestSaveLoad()
 	{
-		[Test(Description = "Тест сохранения и загрузки списка заметок")]
-		public void TestSaveLoad()
+		var expected = new Project();
+
+		expected.AddNote(new Note("First note", "Text", Category.Documents));
+		expected.AddNote(new Note("Second note", "Text", Category.People));
+		ProjectManager.SetPath(@".\NotesApp.txt");
+		ProjectManager.Save(expected);
+		var actual = ProjectManager.Load();
+
+
+		Assert.AreEqual(expected.ToString(), actual.ToString());
+
+		if (File.Exists(ProjectManager.GetPath()))
 		{
-			var expected = new Project();
-
-			expected.AddNote(new Note("First note", "Text", Category.Documents));
-			expected.AddNote(new Note("Second note", "Text", Category.People));
-			ProjectManager.SetPath(@".\NotesApp.txt");
-			ProjectManager.Save(expected);
-			var actual = ProjectManager.Load();
-
-
-			Assert.AreEqual(expected.ToString(), actual.ToString());
-
-			if (File.Exists(ProjectManager.GetPath()))
-			{
-				File.Delete(ProjectManager.GetPath());
-			}
+			File.Delete(ProjectManager.GetPath());
 		}
+	}
 
-		[Test(Description = "Тест геттера и сеттера _path")]
-		public void TestGetSetPath()
-		{
-			var expected = @"..\Notes.txt";
+	[Test(Description = "Тест геттера и сеттера _path")]
+	public void TestGetSetPath()
+	{
+		var expected = @"..\Notes.txt";
 
-			ProjectManager.SetPath(expected);
-			var actual = ProjectManager.GetPath();
+		ProjectManager.SetPath(expected);
+		var actual = ProjectManager.GetPath();
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
 	}
 }
--- NotesAppUnitTests/ProjectTest.cs
+++ NotesAppUnitTests/ProjectTest.cs
@@ -4,105 +4,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using NotesAppClasses;
 
-namespace NotesApp.UnitTests
+namespace NotesApp.UnitTests;
+
+[TestFixture]
+public class ProjectTest
 {
-	[TestFixture]
-	public class ProjectTest
+	[Test(Description = "Тест геттера кол-ва заметок в списке")]
+	public void TestNotesCountsGet()
 	{
-		[Test(Description = "Тест геттера кол-ва заметок в списке")]
-		public void TestNotesCountsGet()
-		{
-			var expected = 0;
+		var expected = 0;
 
-			var project = new Project();
-			var actual = project.GetNoteCount();
+		var project = new Project();
+		var actual = project.GetNoteCount();
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест функций добавления заметки в конец списка и получения заметки по индексу")]
-		public void TestNotesGetByIndexSetInTheEnd()
-		{
-			var expected = new Note("Note name", "Note text", Category.Finances);
+	[Test(Description = "Тест функций добавления заметки в конец списка и получения заметки по индексу")]
+	public void TestNotesGetByIndexSetInTheEnd()
+	{
+		var expected = new Note("Note name", "Note text", Category.Finances);
 
-			var project = new Project();
-			project.AddNote(expected);
-			var actual = project.GetNoteByIndex(0);
+		var project = new Project();
+		project.AddNote(expected);
+		var actual = project.GetNoteByIndex(0);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест функции добавления элемента по индексу в список и получения заметки по индексу")]
-		public void TestNotesSetGetByIndex()
-		{
-			var expected = new Note("Edited note", "Note text", Category.Finances);
+	[Test(Description = "Тест функции добавления элемента по индексу в список и получения заметки по индексу")]
+	public void TestNotesSetGetByIndex()
+	{
+		var expected = new Note("Edited note", "Note text", Category.Finances);
 
-			var project = new Project();
-			project.AddNote(new Note("Note", "Text", Category.Home));
-			project.AddNote(new Note("Note", "Text", Category.Home));
-			project.AddNote(new Note("Note", "Text", Category.Home));
-			project.SetNoteByIndex(1, expected);
-			var actual = project.GetNoteByIndex(1);
+		var project = new Project();
+		project.AddNote(new Note("Note", "Text", Category.Home));
+		project.AddNote(new Note("Note", "Text", Category.Home));
+		project.AddNote(new Note("Note", "Text", Category.Home));
+		project.SetNoteByIndex(1, expected);
+		var actual = project.GetNoteByIndex(1);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест функции удаления заметки")]
-		public void TestRemoveNote()
-		{
-			var expected = new Note("Third note", "Text", Category.Finances);
+	[Test(Description = "Тест функции удаления заметки")]
+	public void TestRemoveNote()
+	{
+		var expected = new Note("Third note", "Text", Category.Finances);
 
-			var project = new Project();
-			project.AddNote(new Note("First note", "Text", Category.People));
-			project.AddNote(new Note("Second note", "Text", Category.People));
-			project.AddNote(expected);
-			project.RemoveNote(1);
-			var actual = project.GetNoteByIndex(1);
+		var project = new Project();
+		project.AddNote(new Note("First note", "Text", Category.People));
+		project.AddNote(new Note("Second note", "Text", Category.People));
+		project.AddNote(expected);
+		project.RemoveNote(1);
+		var actual = project.GetNoteByIndex(1);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест сортировки заметок по времени их последнего редактирования")]
-		public void TestSortNotesByDate()
-		{
-			var expected = new Note("Third note", "Edited text", Category.Finances);
+	[Test(Description = "Тест сортировки заметок по времени их последнего редактирования")]
+	public void TestSortNotesByDate()
+	{
+		var expected = new Note("Third note", "Edited text", Category.Finances);
 
-			var project = new Project();
-			project.AddNote(new Note("First note", "Text", Category.People));
-			project.AddNote(new Note("Second note", "Text", Category.People));
-			project.AddNote(expected);
-			project.GetNoteByIndex(2).SetText("Edited text");
-			project.SortNotesByDate();
-			var actual = project.GetNoteByIndex(0);
+		var project = new Project();
+		project.AddNote(new Note("First note", "Text", Category.People));
+		project.AddNote(new Note("Second note", "Text", Category.People));
+		project.AddNote(expected);
+		project.GetNoteByIndex(2).SetText("Edited text");
+		project.SortNotesByDate();
+		var actual = project.GetNoteByIndex(0);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест функции получения списка заметок категории Default")]
-		public void TestGetNotesWithCategoryDefault()
-		{
-			var expected = new Note("First note", "Text", Category.People);
+	[Test(Description = "Тест функции получения списка заметок категории Default")]
+	public void TestGetNotesWithCategoryDefault()
+	{
+		var expected = new Note("First note", "Text", Category.People);
 
-			var project = new Project();
-			project.AddNote(expected);
-			var actual = project.GetNotesWithCategory(Category.Default).GetNoteByIndex(0);
+		var project = new Project();
+		project.AddNote(expected);
+		var actual = project.GetNotesWithCategory(Category.Default).GetNoteByIndex(0);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
+	}
 
-		[Test(Description = "Тест функции олучения списка заметок заданной категории")]
-		public void TestGetNotesWithCategory()
-		{
-			var expected = new Note("First note", "Text", Category.Home);
+	[Test(Description = "Тест функции олучения списка заметок заданной категории")]
+	public void TestGetNotesWithCategory()
+	{
+		var expected = new Note("First note", "Text", Category.Home);
 
-			var project = new Project();
-			project.AddNote(expected);
-			project.AddNote(new Note("Second note", "Text", Category.People));
-			var actual = project.GetNotesWithCategory(Category.Home).GetNoteByIndex(0);
+		var project = new Project();
+		project.AddNote(expected);
+		project.AddNote(new Note("Second note", "Text", Category.People));
+		var actual = project.GetNotesWithCategory(Category.Home).GetNoteByIndex(0);
 
-			Assert.AreEqual(expected, actual);
-		}
+		Assert.AreEqual(expected, actual);
 	}
 }
